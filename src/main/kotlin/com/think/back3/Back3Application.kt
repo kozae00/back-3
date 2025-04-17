28c65b8350590a0e6a20428a8886d9c999ccebe3
kozae00 <rhwogus777@gmail.com>
2025-04-17 17:02:40 +0900

    init

---
+++ src/main/kotlin/com/think/back3/Back3Application.kt
@@ -0,0 +1,11 @@
+package com.think.back3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Back3Application
+
+fun main(args: Array<String>) {
+    runApplication<Back3Application>(*args)
+}
